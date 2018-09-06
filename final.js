a752afabb3632c37b23e36df0f100782406b5430
David <david.bennett@ncf.edu>
2018-09-06 13:18:18 -0400

    Coding challenge completed.

---
+++ final.js
@@ -0,0 +1,197 @@
+
+/////////////////////////////////
+// CODING CHALLENGE
+
+/*
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+All the report data should be printed to the console.
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+*/
+
+
+// construct common class
+class Element {
+
+	constructor(name, year) {
+		this.name = name;
+		this.year = year;
+	}
+
+	findAge(year) {
+		this.age = new Date().getFullYear() - year;
+	}
+
+}
+
+
+
+class Park extends Element {
+	constructor(name, year, area, treeCount) {
+
+		super(name, year);
+
+		this.area = area;
+		this.treeCount = treeCount;
+	}
+
+	findDensity() {
+		this.density = this.treeCount / this.area;
+		console.log(this.density);
+	}
+
+}
+
+
+
+class Street extends Element {
+	constructor(name, year, length, size = 3) {
+
+		super(name, year);
+
+		this.length = length;
+		this.size = size;
+	}
+
+	streetClass() {
+
+		const classification = new Map();
+	  classification.set(1, 'tiny');
+	  classification.set(2, 'small');
+	  classification.set(3, 'normal');
+	  classification.set(4, 'big');
+	  classification.set(5, 'huge');
+
+	  // note how fucking cool this is.
+	  this.size = classification.get(this.size);
+	  console.log(`${this.name}'s size classification is ${this.size}.`);
+	}
+
+}
+
+
+
+const parks 	= [
+	// name, year, area, treeCount
+	new Park("Cherrywood", 1965, 900, 250),
+	new Park("Vinegrove", 1956, 1200, 600),
+	new Park("Dirtpath", 1912, 2400, 1050)	
+];
+
+const streets = [
+	// name, year, length
+	new Street("Lincoln", 1907, 20, 2),
+	new Street("Kennedy", 1982, 35, 3),
+	new Street("King", 1992, 65, 4),
+	new Street("Church", 1900)
+];
+
+
+console.log(parks);
+console.log(streets);
+
+
+
+function treeDensity() {
+	parks.forEach( function(park) {
+		let treeCount = park.treeCount;
+		let area = park.area;
+		let name = park.name;
+		console.log(`The tree density of ${name} is ${area / treeCount}.`);
+	});
+}
+
+
+function findTrees() {
+		parks.forEach( function(park) {
+		let treeCount = park.treeCount;
+		let name = park.name;
+
+		if (treeCount > 1000) {
+			console.log(`The tree count of ${name} is over 1000 (${treeCount}).`);
+		}
+	});
+}
+
+function streetInfo() {
+
+	let totalLength = 0;
+	let averageLength = 0;
+	const streetCount = streets.length;
+
+	streets.forEach( function(street) {
+
+		let length = street.length;
+
+		if (length !== undefined) {	
+			totalLength += length;
+		}
+
+	});
+
+	console.log(`The total length of all streets is ${totalLength}.`);
+
+	averageLength = totalLength / streetCount;
+
+	console.log(`The average length? Why, ${averageLength}. Weird question.`);
+
+}
+
+
+function streetClasses() {
+	streets.forEach( function(street) {
+
+		street.streetClass();
+
+	});
+}
+
+
+function averageParkAge() {
+
+	let totalAge = 0;
+	const parkCount = parks.length;
+
+	console.log(`The amount of parks is ${parkCount}.`);
+
+	parks.forEach( function(park) {
+
+		let name = park.name;
+		let year = park.year;
+		let age = new Date().getFullYear() - year;
+
+		console.log(`${name} park is ${age} years old.`);
+
+		totalAge += age;
+		console.log(totalAge);
+
+	});
+
+	let averageAge = totalAge / parkCount;
+	console.log(`The average age of all parks is ${averageAge}.`);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
